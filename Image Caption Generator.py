1ad1a014598d1b3e1041f164e3bcf6aede63d9c7
sevangeline2020 <92109516+sevangeline2020@users.noreply.github.com>
2022-07-17 18:34:37 +0530

    Create Image Caption Generator.py

---
+++ Image Caption Generator.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header("Image Caption Generator")
+st.write("Image caption generator is a model which generates caption based on the features present in the input image.")
