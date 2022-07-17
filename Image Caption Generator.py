5da71213166d894592e60f689bb66e855aaa4539
sevangeline2020 <92109516+sevangeline2020@users.noreply.github.com>
2022-07-17 18:41:08 +0530

    Update Image Caption Generator.py

--- Image Caption Generator.py
+++ Image Caption Generator.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.header("Image Caption Generator")
 st.write("Image caption generator is a model which generates caption based on the features present in the input image.")
+st.subheader("DATASET")
+st.write("The dataset used here is the FLICKR 8K which consists of around 8091 images along with 5 captions for each images. If you have a powerful system with more than 16 GB RAM and a graphic card with more than 4 GB of memory, you can try to take FLICKR 30K which has around 30,000 images with captions.")
+st.image
+             
